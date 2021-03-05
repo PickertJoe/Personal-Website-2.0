1f8430f051a2fb4c77e8f40b84eef5fe81e03984
Joe <pickertjoe@gmail.com>
2021-03-05 16:57:14 -0600

    Basic navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,46 +1,60 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import SideDrawer from "../components/SideDrawer"
+import { AppBar, Toolbar, makeStyles, IconButton, Container, List, ListItem, ListItemText, Hidden } from "@material-ui/core"
+import { Home } from "@material-ui/icons";
+
+const useStyles = makeStyles({
+  navDisplayFlex: {
+    display: `flex`,
+    justifyContent: `space-between`
+  },
+  navbarDisplayFlex: {
+    display: `flex`,
+    justifyContent: `space-between`
+  },
+  linkText: {
+    textDecoration: `none`,
+    textTransform: `uppercase`,
+    color: `white`
+  }
+});
 
 const navLinks = [
   { title: `about me`, path: `/about` },
   { title: `work`, path: `/work` },
   { title: `blog`, path: `/blog` },
   { title: `contact`, path: `/contact` },
-  { title: `contact`, path: `/contact` },
 ]
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+  const classes = useStyles();
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Container maxWidth="lg"className={classes.navbarDisplayFlex}>
+          <IconButton edge="start" color="inherit" aria-label="home">
+            <Home fontSize="large" />
+          </IconButton>
+          <Hidden smDown>
+            <List component="nav" aria-labelledby="main navigation" className={classes.navDisplayFlex}>
+              {navLinks.map(({ title, path }) => (
+                <Link to={path} className={classes.linkText}>
+                  <ListItem button>
+                    <ListItemText primary={title} />
+                  </ListItem>
+                </Link>
+              ))}          
+            </List>
+          </Hidden>
+          <Hidden mdUp>
+            <SideDrawer navLinks={navLinks}/>
+          </Hidden>
+        </Container>
+      </Toolbar>
+    </AppBar>
+  )
 }
 
 Header.defaultProps = {
